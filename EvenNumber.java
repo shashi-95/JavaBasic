73479602eabe648e3a71ecd05eea56af3dfd4717
shashi kant <skant5593@gmail.com>
2021-05-23 14:14:52 +0530

    New Folder added

---
+++ EvenNumber.java
@@ -0,0 +1,10 @@
+public class EvenNumber {
+public static  void main() {
+   for(int i=0; i<10; i++) {
+	   if((i&1) == 0) {
+		   System.out.println("Even NUmber" +i);
+	   }
+		   
+   }
+}
+}
--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,7 @@
 public class Helloworld {
 public static void main() {
  System.out.println("hello world");
+ System.out.println("this is checking");
 }
 
 }
