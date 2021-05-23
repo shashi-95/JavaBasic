76e6131d1e6309623590474660e0729bdfe915b7
shashi kant <skant5593@gmail.com>
2021-05-23 15:01:43 +0530

    New File added

--- EvenNumber.java
+++ EvenNumber.java
@@ -1,5 +1,5 @@
 public class EvenNumber {
-public static  void main() {
+public static  void main(String args[]) {
    for(int i=0; i<10; i++) {
 	   if((i&1) == 0) {
 		   System.out.println("Even NUmber" +i);
---
+++ NewHello.java
@@ -0,0 +1,6 @@
+public class NewHello {
+  public static void main(String args[]) {
+		System.out.println("hello shashi");
+		System.out.println("New number shashi");
+	}
+}
